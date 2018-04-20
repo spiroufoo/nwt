fe3e6b502917ef05fcadee9d6fcaf264c28a1983
spiroufoo <spiroufoo@gmail.com>
2018-04-20 02:27:40 +0200

    error handling

--- public/js/nwt.js
+++ public/js/nwt.js
@@ -13,11 +13,16 @@
                     type: "GET",
                     url: rest_url,
                     success: function(data) {
-                        $("#result").html(
-                            '<div class="box"><ul><li>' + data.repo1.full_name + '</li><li>Forks: ' + data.repo1.forks_count + '</li><li>Stars: ' + data.repo1.stargazers_count + '</li><li>Watchers: ' + data.repo1.subscribers_count + '</li><li>Latest release: ' + data.repo1.pushed_at + '</li><li>Open pulls: ' + data.repo1.open_pulls + '</li><li>Closed pulls: ' + data.repo1.closed_pulls + '</li></ul></div>' + 
-                            '<div class="box"><ul><li>' + data.repo2.full_name + '</li><li>Forks: ' + data.repo2.forks_count + '</li><li>Stars: ' + data.repo2.stargazers_count + '</li><li>Watchers: ' + data.repo2.subscribers_count + '</li><li>Latest release: ' + data.repo2.pushed_at + '</li><li>Open pulls: ' + data.repo2.open_pulls + '</li><li>Closed pulls: ' + data.repo2.closed_pulls + '</li></ul></div>' +
-                            '<div class="box"><ul><li>Comparison</li><li>Popularity: ' + data.comparison.diff_popularity + '</li><li>Activity: ' + data.comparison.diff_activity + '</li><li>Size: ' + data.comparison.diff_size + '</li></ul></div>'
-                        );
+                        if(data.message) {
+                            $("#result").html(data.message);
+                        }
+                        else {
+                            $("#result").html(
+                                '<div class="box"><ul><li>' + data.repo1.full_name + '</li><li>Forks: ' + data.repo1.forks_count + '</li><li>Stars: ' + data.repo1.stargazers_count + '</li><li>Watchers: ' + data.repo1.subscribers_count + '</li><li>Latest release: ' + data.repo1.pushed_at + '</li><li>Open pulls: ' + data.repo1.open_pulls + '</li><li>Closed pulls: ' + data.repo1.closed_pulls + '</li></ul></div>' + 
+                                '<div class="box"><ul><li>' + data.repo2.full_name + '</li><li>Forks: ' + data.repo2.forks_count + '</li><li>Stars: ' + data.repo2.stargazers_count + '</li><li>Watchers: ' + data.repo2.subscribers_count + '</li><li>Latest release: ' + data.repo2.pushed_at + '</li><li>Open pulls: ' + data.repo2.open_pulls + '</li><li>Closed pulls: ' + data.repo2.closed_pulls + '</li></ul></div>' +
+                                '<div class="box"><ul><li>Comparison</li><li>Popularity: ' + data.comparison.diff_popularity + '</li><li>Activity: ' + data.comparison.diff_activity + '</li><li>Size: ' + data.comparison.diff_size + '</li></ul></div>'
+                            );
+                        }
                     },
                     error: function(err) {
                         $("#result").text("API Error");
